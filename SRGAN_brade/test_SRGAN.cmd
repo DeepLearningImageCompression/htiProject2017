1c97120ea75a29ed42bb773aa13df0758e675a81
jules <julesabdo@gmail.com>
2018-01-27 11:53:40 +0100

    all the files needed to run Brade's SRGAN with the pretrained SRGAN given and the SRGAN we trained for faces

---
+++ SRGAN_brade/test_SRGAN.cmd
@@ -0,0 +1,14 @@
+python -m python main.py
+    --output_dir ./result/ \
+    --summary_dir ./result/log/ \
+    --mode test \
+    --is_training False \
+    --task SRGAN \
+    --batch_size 16 \
+    --input_dir_LR ./data/test_LR/ \
+    --input_dir_HR ./data/test_HR/ \
+    --num_resblock 16 \
+    --perceptual_mode VGG54 \
+    --pre_trained_model True \
+    --checkpoint ./SRGAN_pre-trained/model-200000
+
